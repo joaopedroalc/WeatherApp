431853f31cdc0d3df6b873aa90e7192993a54e9c
joaopedrocode <joaopedro.ufma@gmail.com>
2022-05-01 10:05:07 -0300

    url base migrate http to https

--- src/Api.tsx
+++ src/Api.tsx
@@ -2,7 +2,7 @@ const API_KEY = '865c34b82ed04588983163717223004';
 
 const Api = async (city : string) => {
     try {
-      const baseURL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
+      const baseURL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`
       const fetchResponse = await fetch(baseURL)
       const data =await fetchResponse.json()
       return data;
